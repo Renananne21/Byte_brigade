2a2789829158af40d4c2a127511975bdee84383c
derak-isaack <oderaisaack@gmail.com>
2024-11-14 23:36:54 +0300

    added ICP payment via alchemy pay

--- .kybra/sentix_backend/python_source/main.py
+++ .kybra/sentix_backend/python_source/main.py
@@ -4,4 +4,3 @@ from events import create_event, get_all_events, get_event_by_id, create_user
 from ticket_mgt import buy_ticket, resale_ticket, buy_resale_ticket, get_ticket
 from token_mgt import reward_tokens, get_user_tokens, spend_tokens
 from models import Event, Ticket, Tokens
-
